8673e2fc35bb81fc0a49bf800ab70f08bb3ebd33
Unknown <e.escher@rubika-edu.com>
2018-05-08 18:55:05 +0200

    init setup

---
+++ Assets/scripts/DoubleClickZoom.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTS_Cam;
+
+public class DoubleClickZoom : MonoBehaviour {
+
+    private bool firstClick = false;
+    private bool isTimerRunning = false;
+    private float timerClick = 0;
+    [SerializeField] float delayBetweenClick = 0.3f;
+
+
+    private Camera mainCamera;
+    private RTS_Camera rtsCam;
+    private float zoomMax;
+    [SerializeField] float delayTransition = 0.5f;
+    private static Transform transformTransition;
+    private float timerTransition = 0;
+    [SerializeField] float distanceBetweenCameraAndTarget = -2;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        rtsCam = mainCamera.GetComponent<RTS_Camera>();
+        zoomMax = rtsCam.minHeight;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //DoubleClick();
+        TransitionIntoTarget();
+	}
+
+    private void DoubleClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!firstClick)
+            {
+                firstClick = true;
+                timerClick = 0;
+            }
+            else
+            {
+                firstClick = false;
+
+                transformTransition = transform;
+                timerTransition = 0;
+            }
+        }
+
+        if (firstClick)
+        {
+            timerClick += Time.deltaTime;
+            if (timerClick > delayBetweenClick)
+            {
+                firstClick = false;
+            }
+        }
+    }
+
+    private void TransitionIntoTarget()
+    {
+        if(transformTransition != null && ReferenceEquals(transformTransition, transform))
+        {
+            timerTransition += Time.deltaTime;
+            if(timerTransition <= delayBetweenClick)
+            {
+                float valLerp = timerTransition / delayBetweenClick;
+                rtsCam.SetZoomPos = 0;
+                mainCamera.transform.position = new Vector3(Mathf.Lerp(mainCamera.transform.position.x, transform.position.x, valLerp), Mathf.Lerp(mainCamera.transform.position.y, zoomMax, valLerp), Mathf.Lerp(mainCamera.transform.position.z, transform.position.z + distanceBetweenCameraAndTarget, valLerp));
+
+            }
+            else
+            {
+                transformTransition = null;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        DoubleClick();
+    }
+}
---
+++ Assets/scripts/Soldier.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Soldier : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 posDepart;
+    private Camera mainCamera;
+    public LayerMask layerDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        print("drag begin");
+        posDepart = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Ray ray = eventData.pressEventCamera.ScreenPointToRay(eventData.position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, float.MaxValue, layerDrag))
+            transform.position = hit.point.WithY(0.3f);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, mainCamera.transform.forward, out hit) && hit.transform.tag == "Spot")
+        {
+            transform.position = hit.transform.position.WithY(0.3f);
+        }
+        else
+        {
+            transform.position = posDepart;
+        }
+    }
+
+    // Use this for initialization
+    void Awake () {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
