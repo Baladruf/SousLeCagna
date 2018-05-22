6d65a640111a9d18f646adfe7bdf8400e173b41f
Unknown <e.escher@rubika-edu.com>
2018-05-22 16:17:11 +0200

    up

---
+++ Assets/scripts/EnumDefine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumDefine {
+
+	public enum RankMilitary
+    {
+
+    }
+	
+	public enum Perks
+    {
+
+    }
+}
---
+++ Assets/scripts/Spot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        GameManager.instance.spotSelected = this;
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Soldier soldier = GameManager.instance.soldierSelected;
+            if(soldier != null)
+            {
+
+            }
+        }
+    }
+}
